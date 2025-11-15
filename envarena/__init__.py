efc2bf4868850dd040af8470335349f9995ceb71
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2025-11-16 02:08:09 +0530

    first time for creating framework??

--- envarena/__init__.py
+++ envarena/__init__.py
@@ -1,8 +1,6 @@
 from .wrappers.OllamaWrapper import OllamaWrapper
 from .ui.dashboard import Dashboard
-from .envs.GuessTheNumber.env import GuessTheNumber
-from .envs.MazeNavigation.env import MazeNavigation
-from .envs.SocialQA.env import SocialQA
-from .envs.BoolQ.env import boolq
+
+
 
 from .simulator import Simulator
